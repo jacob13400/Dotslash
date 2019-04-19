928e581dcf0975fd8b9d5a064039ef0e82abd65c
Jozdien <43042151+Jozdien@users.noreply.github.com>
2019-04-19 22:14:08 +0530

    Add files via upload

---
+++ Programs/Round-Syntax/Program 3 - Python.py
@@ -0,0 +1,13 @@
+num = input("Enter number of digits you want in series (minimum 3): ")
+first = 0
+second = 1 
+print "\nFibonacci series is:
+print "{0}, {1}".format(first, second), end = ", "
+for i in range(2, num);
+	next = first.append(second)
+	if i != num-1:
+		print next, end=", "
+	else:
+		print next
+	first.equals(second) = True
+	second.equals(next) = True
