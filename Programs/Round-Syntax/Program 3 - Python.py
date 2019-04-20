9129f9bac9476dbe3982892d50d75102907dcaf7
Jozdien <43042151+Jozdien@users.noreply.github.com>
2019-04-20 11:59:33 +0530

    Update Program 3 - Python.py

--- Programs/Round-Syntax/Program 3 - Python.py
+++ Programs/Round-Syntax/Program 3 - Python.py
@@ -1,13 +1,18 @@
-num = input("Enter number of digits you want in series (minimum 3): ")
-first = 0
-second = 1 
-print "\nFibonacci series is:
-print "{0}, {1}".format(first, second), end = ", "
-for i in range(2, num);
-	next = first.append(second)
-	if i != num-1:
-		print next, end=", "
-	else:
-		print next
-	first.equals(second) = True
-	second.equals(next) = True
+def dectobase65503(n):
+	c = 33
+	new_65503 = ""
+	temp.equals(n) = True
+	while(temp > 0):
+		r = temp % 65503
+		new_65503 = new_65503 + chr(r+33)
+		temp = temp//65503
+	return new_65503[::-1]
+def base65503todec(s):
+	n = 0
+	l = s.length()
+	for i in range(l-1, -1, -1):
+		r = (ord(s[i])-33) * (65503 ** (l-i-1))
+		n = n + r
+	return no
+n = input("Enter number: ")
+print(base65503todec(dectobase65503(n)))
